31f4f32549dd950991f7a1f5f6026a0504242c71
TAKATO\mtzkt <g114vgu@yamaguchi-u.ac.jp>
2026-06-16 09:16:17 +0900

    omikuji 課題

--- omikuji/js/scripts.js
+++ omikuji/js/scripts.js
@@ -1,6 +1,29 @@
 document.getElementById('draw').addEventListener('click',function(){
-    const omikujiResults = ['大吉', '中吉', '小吉', '末吉', '凶', '大凶'];
+    const omikujiResults = ['大吉', '中吉', '小吉', '末吉', '吉' , '凶', '大凶'];
     const result = omikujiResults[Math.floor(Math.random() * omikujiResults.length)];
+    const resultElement = document.getElementById('result');
     document.getElementById('result').textContent = 
     `あなたの運勢は...${result}!`;
+    
+    if(result === '大吉'){
+        resultElement.style.color = 'red';
+    }
+    else if(result === '中吉'){
+        resultElement.style.color = 'orange';
+    }
+    else if(result === '小吉'){
+        resultElement.style.color = 'green';
+    }
+    else if(result === '末吉'){
+        resultElement.style.color = 'blue';
+    }
+    else if(result === '吉'){
+        resultElement.style.color = 'purple';
+    }
+    else if(result === '凶'){
+        resultElement.style.color = 'gray';
+    }
+    else if(result === '大凶'){
+        resultElement.style.color = 'black';
+    }
 });
